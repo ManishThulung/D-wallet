ddf84eb75f50b6c740849f01dbcf6832056efd5d
ManishThulung <manish.thulung58@gmail.com>
2024-01-23 16:44:03 +0545

    controller setup

--- chromeapi/Api/Routers/accountRouter.js
+++ chromeapi/Api/Routers/accountRouter.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const { authController } = require("../Controllers/authController");
+
+const router = express.Router();
+
+router.get("/all-account", authController.allAccount);
+router.post("/create-account", authController.createAccount);
+
+module.exports = router;
---
+++ chromeapi/Api/Routers/tokenRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { authController } = require("../Controllers/authController");
+
+const router = express.Router();
+
+router.get("/all-token", authController.allToken);
+router.post("/create-token", authController.createToken);
+
+module.exports = router;
